04dd909e040d20b1c66cd49ebac6211dd9ad96b1
Nathercia <dev.nathercia@gmail.com>
2022-07-18 17:00:02 -0300

    criado o arquivo mongooseConnect.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+require('dontenv').config()
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect()
+
+module.exports = app
+
